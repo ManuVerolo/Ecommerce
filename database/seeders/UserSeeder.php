74a50dfa668f61f143fb54bc81c3cd2a380ac824
Manuel Verolo <mverolo@axter.com.ar>
2022-05-25 20:38:48 -0300

    Agrega laravel permissions

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-
+use Spatie\Permission\Models\Role;
 class UserSeeder extends Seeder
 {
     /**
@@ -14,10 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        $role = Role::create(['name' => 'admin']);
+
         User::create([
             'name' => 'Manuel Verolo',
             'email' => 'manuelverolo@gmail.com',
             'password' => bcrypt('12345678')
-        ]);
+        ])->assignRole('admin');
     }
 }
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -41,10 +41,11 @@
                             Mis pedidos
                         </x-jet-dropdown-link>
 
-                        
-                        <x-jet-dropdown-link href="{{ route('admin.index') }}">
-                            Panel Administración
-                        </x-jet-dropdown-link>
+                        @role('admin')
+                            <x-jet-dropdown-link href="{{ route('admin.index') }}">
+                                Panel Administración
+                            </x-jet-dropdown-link>
+                        @endrole
 
                         <div class="border-t border-gray-100"></div>
 
